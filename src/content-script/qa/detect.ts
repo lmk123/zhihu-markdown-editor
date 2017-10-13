050953b65207fa6f22d0345317236e355b3827d0
lmk123 <me@limingkai.cn>
2017-10-13 15:50:48 +0800

    修改问答页中知乎编辑器的检测方式

--- src/content-script/qa/detect.ts
+++ src/content-script/qa/detect.ts
@@ -1,54 +1,24 @@
 // 检测知乎问答的编辑器
 import getZHEditor from '../share/get-zh-editor'
 
-export default function (): Promise<TInfo> {
+export default function (onEditorShow: (editor: HTMLDivElement) => void) {
   // 如果问题还没有回答过，则知乎编辑器是一开始就有的
-  const info = getInfo()
-  if (info) {
-    return Promise.resolve(info)
-  }
-
-  // 否则需要等用户点了「修改」链接之后，知乎编辑器才会出现
-  return new Promise((resolve, reject) => {
-
-    function handler (event: MouseEvent) {
-      const editLink = (event.target as Element).closest('.AnswerItem-editButton')
-      if (editLink) {
-        document.removeEventListener('click', handler)
-        const info = getInfo()
-        if (info) {
-          resolve(info)
-        } else {
-          reject(new Error('无法检测到知乎编辑器。'))
-        }
-      }
-    }
-
-    document.addEventListener('click', handler)
-  })
-}
-
-export type TInfo = {
-  id: string
-  aid?: string
-  editor: HTMLDivElement
-}
-
-const qaReg = /\d+/g
-
-function getInfo (): TInfo | void | never {
   const editor = getZHEditor()
   if (editor) {
-    const url = new URL(window.location.href)
-    const match = url.pathname.match(qaReg)
-    if (match) {
-      return {
-        id: match[0], // 问题的 id
-        aid: match[1], // 答案的 id
-        editor
-      }
-    } else {
-      throw new Error('无法解析知乎问答的 id，可能是知乎的链接格式改变了。')
-    }
+    onEditorShow(editor)
+    return
   }
+
+  // 如果问题已经回答过了，则每次用户点了「修改」链接之后，知乎编辑器都会出现
+  document.addEventListener('click', function (event: MouseEvent) {
+    const editLink = (event.target as Element).closest('.AnswerItem-editButton')
+    if (editLink) {
+      const editor = getZHEditor()
+      if (editor) {
+        onEditorShow(editor)
+      } else {
+        throw new Error('无法检测到知乎编辑器，可能是页面结构发生了变化。')
+      }
+    }
+  })
 }
--- src/content-script/qa/index.ts
+++ src/content-script/qa/index.ts
@@ -1,7 +1,36 @@
 import detect from './detect'
 import MDE from '../share/MarkDownEditor'
 
-detect().then(info => {
-  const mde = new MDE()
-  ;(info.editor.parentElement as Element).appendChild(mde.textarea)
+type TInfo = {
+  id: string
+  aid?: string
+}
+
+const qaReg = /\d+/g
+let mde: MDE
+let info: TInfo
+
+function parseQA () {
+  const url = new URL(window.location.href)
+  const match = url.pathname.match(qaReg)
+  if (match) {
+    return {
+      id: match[0], // 问题的 id
+      aid: match[1] // 答案的 id
+    }
+  } else {
+    throw new Error('无法检测知乎问答信息，可能是知乎链接结构发生了变化。')
+  }
+}
+
+detect(editor => {
+  if (!mde) {
+    mde = new MDE()
+  }
+
+  if (!info) {
+    info = parseQA()
+  }
+
+  (editor.parentElement as Element).appendChild(mde.textarea)
 })
